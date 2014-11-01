08c0300df48171e01cfe87ef9251d0087d71f41a
JacobFire <jacobfiregorilla@gmail.com>
2014-11-01 16:46:54 +0200

    Form through Entity

--- src/Sirius/MeetBundle/Controller/LoginController.php
+++ src/Sirius/MeetBundle/Controller/LoginController.php
@@ -2,13 +2,28 @@
 
 namespace Sirius\MeetBundle\Controller;
 
+use Sirius\MeetBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LoginController extends Controller
 {
     public function indexAction()
     {
+        // create a task and give it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
 
-        return $this->render('SiriusMeetBundle:Login:index.html.twig');
+        $form = $this->createFormBuilder($task)
+            ->add('task', 'text')
+            ->add('dueDate', 'date')
+            ->add('save', 'submit', array('label' => 'Create Task'))
+            ->getForm();
+
+        return $this->render('SiriusMeetBundle:Login:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+        //return $this->render('SiriusMeetBundle:Login:index.html.twig');
     }
 }
